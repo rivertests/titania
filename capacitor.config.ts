af1b929670d00e79237fe4a14965541a9f73f749
semtetog <brennodmoraes@gmail.com>
2025-08-23 22:40:38 -0300

    Primeiro commit do projeto iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.inteligenciatitan.app',
+  appName: 'Titan IA',
+  webDir: 'www', // Deixe isso, mesmo que a pasta esteja vazia
+  plugins: {
+    EdgeToEdge: {
+      backgroundColor: '#101212'
+    },
+  },
+};
+
+export default config;
