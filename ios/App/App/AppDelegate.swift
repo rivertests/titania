4892d090ff5230e73d7656cb2d8a87fc5aa78a6b
semtetog <brennodmoraes@gmail.com>
2025-08-25 17:24:45 -0300

    Reset: Usa Podfile padrão do Capacitor e AppDelegate mínimo

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -2,16 +2,7 @@ import UIKit
 import Capacitor
 
 class AppDelegate: CAPAppDelegate {
-
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
-    }
-
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
-
-    override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 }
